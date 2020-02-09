36d87af9f8132ef7ec4c302e7cf97bfaef497e10
Thomas Boyt <thomas.boyt@classpass.com>
2020-02-09 02:25:51 -0500

    remove unused error

--- src/errors.ts
+++ src/errors.ts
@@ -1,16 +1,5 @@
 import { ErrorObject } from 'ajv';
 
-export class ParamValidationError extends Error {
-  key: string;
-  error: ErrorObject;
-
-  constructor(key: string, error: ErrorObject) {
-    super('Failed to parse param');
-    this.key = key;
-    this.error = error;
-  }
-}
-
 export class SchemaValidationError extends Error {
   errors: ErrorObject[];
 
